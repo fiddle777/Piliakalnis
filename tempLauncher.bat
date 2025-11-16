328be6b5ea0b60c5b731aca3d1df1705d7891efe
Deividas Narbutas <d.narbutas@stud.vilniustech.lt>
2025-11-16 13:39:11 +0200

    added JAR;deleated temp files;

--- tempLauncher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set javaPath="C:\Users\Deividas\.jdks\openjdk-24.0.2+12-54\bin"
-set projectPath="C:\Users\Deividas\Desktop\UniS5\Piliakalnis\out\production\Piliakalnis"
-echo OPLIA
-%javaPath%\java.exe -cp %projectPath% Core.Main
-pause
