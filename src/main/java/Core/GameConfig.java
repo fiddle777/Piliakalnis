b2cedc49fd2cf629613e3360e1dfce637685e14f
Deividas Narbutas <d.narbutas@stud.vilniustech.lt>
2025-12-14 19:09:18 +0200

    rebalanced starting stats

--- src/main/java/Core/GameConfig.java
+++ src/main/java/Core/GameConfig.java
@@ -4,10 +4,10 @@ public class GameConfig {
     public static final int START_YEAR = 1200;
     public static final int START_GOLD = 300;
     public static final int START_MORALE = 40;
-    public static final int START_FOOD = 140;
-    public static final int START_POPULATION = 50;
+    public static final int START_FOOD = 180;
+    public static final int START_POPULATION = 40;
     public static final int START_DEFENSE = 20;
-    public static final int START_FAITH = 8;
+    public static final int START_FAITH = 4;
 
     public static final int MAX_MORALE = 100;
     public static final int MAX_FAITH = 100;
